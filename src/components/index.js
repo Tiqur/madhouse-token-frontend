957943472cd4cb4ac7443339680ca5ce9c55716c
Tiqur <Tiqur7@gmail.com>
2021-07-25 03:33:07 -0700

    Added Text component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export {default as Layout} from './layout/main.jsx';
 export {default as Navbar} from './navbar/main.jsx';
+export {default as Text} from './text/main.jsx';
---
+++ src/components/text/main.jsx
@@ -0,0 +1,15 @@
+const Text = (props) => {
+  return (
+    <p style={{...{
+      display: props.inline ? 'inline' : 'block',
+      color: props.color ? props.color : '#D7D7D7',
+      fontWeight: props.weight ? props.weight : 'initial',
+      fontSize: (props.size ? props.size : 'initial') + 'ch',
+      fontFamily: props.family ? props.family : 'Roboto',
+      padding: props.padding ? props.padding : 'initial',
+      margin: props.margin ? props.margin : 'initial'
+    }, ...props.style}} children={props.children} id={props.id}/>
+  )
+}
+
+export default Text;
