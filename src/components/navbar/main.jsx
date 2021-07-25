c0023c293fc8d95d44619a51eb8578fd9e57bf1d
Tiqur <Tiqur7@gmail.com>
2021-07-25 02:51:10 -0700

    Setup navbar content

--- src/components/navbar/main.jsx
+++ src/components/navbar/main.jsx
@@ -1,9 +1,26 @@
 import styles from './styles.module.scss';
+import { Link } from 'react-router-dom';
+
+const NavLink = (props) => {
+  return (
+    <Link to={{pathname: props.to}}>
+      <a>{props.text}</a>
+    </Link>
+  )
+}
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      
+      <div className={styles.logo_container}>
+        <p>MADHOUSE</p>
+      </div>
+      <div className={styles.nav_links_container}>
+        <NavLink to='/home' text='Home'/>
+        <NavLink to='/about' text='About'/>
+        <NavLink to='/charts' text='Charts'/>
+        <NavLink to='/whitepaper' text='Whitepaper'/>
+      </div>
     </div>
   )
 }
