9b3f9be9ee287a1aa1df32d82301754008786874
Tiqur <Tiqur7@gmail.com>
2021-07-25 04:42:41 -0700

    Extend hero beyond layout

--- src/pages/home/main.jsx
+++ src/pages/home/main.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
       <div className={styles.hero_container}>
         <div className={styles.hero_text}>
           <Text family='League Spartan Bold' size={7}>Introducing:</Text>
-          <Text family='League Spartan Bold' size={5.5}>The Madhouse Token:</Text>
+          <Text family='League Spartan Bold' size={5.5}>The Madhouse Token</Text>
           <Text size={2} family='Inter'>THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.</Text>
         </div>
         <img src={LogoPng} style={{marginRight: '4em', height: '400px', width: '400px'}} alt='logo'/>
