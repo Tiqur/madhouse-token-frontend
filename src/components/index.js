717f74cc0956ac345a12b3fb472097efc1c28219
Tiqur <Tiqur7@gmail.com>
2021-07-26 02:12:31 -0700

    Initialize footer component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Navbar} from './navbar/main.jsx';
 export {default as Text} from './text/main.jsx';
 export {default as InfoIcon} from './info_icon/main.jsx';
+export {default as Footer} from './footer/main.jsx';
