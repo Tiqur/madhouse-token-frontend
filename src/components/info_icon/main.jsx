9f64d7d715100648e94b4b08a2c89b1b4f165f65
Tiqur <Tiqur7@gmail.com>
2021-07-25 23:52:13 -0700

    Made icon size dynamic

--- src/components/info_icon/main.jsx
+++ src/components/info_icon/main.jsx
@@ -4,7 +4,7 @@ import { Text } from '../index.js';
 const InfoIcon = (props) => {
   return (
     <div className={styles.icon_container}>
-      <props.src style={{height: '8em', width: '8em'}} fill='#7C7C7C'/>
+      <props.src className={styles.icon} fill='#7C7C7C'/>
       <Text color='#7C7C7C'>{props.info}</Text>
       <Text>{props.value}</Text>
     </div>
