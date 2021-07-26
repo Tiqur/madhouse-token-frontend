696b61a8616d4f9da6b6a35e755e48f24033373b
Tiqur <Tiqur7@gmail.com>
2021-07-26 02:06:31 -0700

    Refactored hero + section layouts

--- src/pages/home/main.jsx
+++ src/pages/home/main.jsx
@@ -19,19 +19,23 @@ const HomePage = () => {
     <div className={styles.main_container}>
       <Navbar/>
       <div className={styles.hero_container}>
-        <div className={styles.hero_text}>
-          <Text family='League Spartan Bold' size={7}>Introducing:</Text>
-          <Text family='League Spartan Bold' size={5.5}>The Madhouse Token</Text>
-          <Text size={2} family='Inter'>THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.</Text>
+        <div className={styles.inner_hero_container}>
+          <div className={styles.hero_text}>
+            <Text family='League Spartan Bold' size={7}>Introducing:</Text>
+            <Text family='League Spartan Bold' size={5.5}>The Madhouse Token</Text>
+            <Text size={2} family='Inter'>THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.</Text>
+          </div>
+          <img src={LogoPng} className={styles.hero_logo} alt='hero_logo'/>
         </div>
-        <img src={LogoPng} className={styles.hero_logo} alt='hero_logo'/>
       </div>
 
-      <div className={styles.content_container}>
+      <Section>
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%'}}>
           <InfoIcon src={DropSvg} info='Liquidity:' value='223,912'/>
           <InfoIcon src={MoneySvg} info='Market Cap:' value='$653,301'/>
           <InfoIcon src={VaultSvg} info='Supply:' value='1,000,000,000'/>
-      </div>
+        </div>
+      </Section>
 
       <Section title='About'>
         <Text>Lorem ipsum damet, consectetur adipiscing Cras augue arcu, luctus in massa quis, efficitur faucibus magna. Vivamus volutpat lacus at elit tempor faucibus.   Phasellus iaculis consequat libero, a malesuada nulla viverra inLorem ipsum dolor sit amet, consectetur adipiscing elit. Cras augue arcu, luctus in massa quis, efficitur faucibus magna. Vivamus volutpat lacus at elit tempor faucibus.   PLorem ipLorem ipsum dolor sit amet, consectetur adipiscing elit. Cras augue arcu, luctus in massa quis, efficitur faucibus magna. Vivamus volutpat lacus at elit tempor faucibus.   Phasellus iaculis consequat libero, a malesuada nulla viverra insum dolor sit amet, consectetur adipiscing elit. Cras augue arcu</Text>
