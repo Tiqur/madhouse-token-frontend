1e3a4b397be271fe395335d5fbadcd280693d0b1
Tiqur <Tiqur7@gmail.com>
2021-07-27 20:02:57 -0700

    Replace tokenomics with 'Deflationomics'

--- src/pages/home/main.jsx
+++ src/pages/home/main.jsx
@@ -65,14 +65,14 @@ const HomePage = () => {
         <Text>The Madhouse token is a unique token with first of its kind tokenomics. We invite you to join us in the new age of how tokenomics was meant to be. We are a team of like minded investors united by a common goal to make the crypto space more geared towards the investor. From our origins in the Markus Madhouse dogecoin youtube community we have bound together our combined brain power and unique talent pool to develop a new era of optimised smart contracts. The Madhouse Token is the first of it’s kind and will be a game changer in the cryto space. You will be able to share and spend it freely with no tax on transfers, the way tokenomics should work. Investors are only taxed when buying and selling to grow the liquidity pool and, most importantly grow their own bags. With our custom tokenomics, Madhouse Tokens can and most certainly will be used as an everyday currency in the future due to our Zero tax on transfers.</Text>
       </Section>
 
-      <Section title='Tokenomics'>
+      <Section title='Deflationomics'>
         <div className={styles.tokenomics_container}>
           <TokenomicsSvg fill='#7C7C7C' className={styles.tokenomics_svg}/>
           <ul className={styles.tokenomics_list}>
-            <li><span className={styles.dot}/>Example Tokenomic</li>
-            <li><span className={styles.dot}/>Example Tokenomic</li>
-            <li><span className={styles.dot}/>Example Tokenomic</li>
-            <li><span className={styles.dot}/>Example Tokenomic</li>
+            <li><span className={styles.dot}/>Example 1</li>
+            <li><span className={styles.dot}/>Example 2</li>
+            <li><span className={styles.dot}/>Example 3</li>
+            <li><span className={styles.dot}/>Example 4</li>
           </ul>
         </div>
       </Section>
