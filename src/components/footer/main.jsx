16c4ac18d93349cb8febfde97c88d2034d3784cf
Tiqur <Tiqur7@gmail.com>
2021-07-29 15:35:24 -0700

    Fixed more accessibility aspects regarding socials

--- src/components/footer/main.jsx
+++ src/components/footer/main.jsx
@@ -10,7 +10,7 @@ import {
 
 const Icon = (props) => {
   return (
-    <a rel="noopener" target='_blank' href={props.to}>
+    <a rel="noopener" target='_blank' aria-label={props.alt} href={props.to}>
       {<props.icon fill='#7C7C7C' alt={props.alt} className={styles.icon}/>}
     </a>
   )
