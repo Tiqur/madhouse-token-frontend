21701fd69d58cba4211b39e92c79af40b63f8ac3
Tiqur <Tiqur7@gmail.com>
2021-07-28 23:41:02 -0700

    Update file permissions

--- update.sh
+++ update.sh
@@ -5,3 +5,4 @@ git pull # Update local repo
 npm install # Update local node modules if necessary
 npm run build # Create optimized build for production
 serve -s build & # Serve optimized build in background process
+chmod +x update.sh # Update file permissions 
