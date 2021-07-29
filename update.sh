e722615edebab1535ba470e1d8c425bc3634581d
Tiqur <Tiqur7@gmail.com>
2021-07-29 01:14:30 -0700

    Updated update script

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 # Update from github and restart
-pkill -9 -f serve # Kill previous running webserver
 git stash # Stash local repo
 git pull # Update local repo
 npm install # Update local node modules if necessary
 npm run build # Create optimized build for production
-serve -s build & # Serve optimized build in background process
 chmod +x update.sh # Update file permissions 
