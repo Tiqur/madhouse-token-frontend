fbb1d66ae0e2998d1e5bab02857360e389fc216c
Tiqur <Tiqur7@gmail.com>
2021-07-28 19:13:36 -0700

    Convert png to webp for better compression

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,6 +8,6 @@ export {ReactComponent as DropSvg} from './drop.svg';
 export {ReactComponent as MoneySvg} from './money.svg';
 export {ReactComponent as VaultSvg} from './vault.svg';
 export {ReactComponent as TokenomicsSvg} from './tokenomics.svg';
-export {default as LogoPng} from './logo.png';
+export {default as LogoPng} from './logo.webp';
 export {default as SpinningCoinMP4} from './spinning_coin.mp4';
 export {default as WhitepaperPdf} from './whitepaper.pdf';
