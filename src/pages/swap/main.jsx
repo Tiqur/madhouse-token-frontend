17b6a3bb69d35a43fd4abbdb264852fa391e73d6
Tiqur <Tiqur7@gmail.com>
2021-08-03 16:16:48 -0700

    Replace swap embed with temp status until launch

--- src/pages/swap/main.jsx
+++ src/pages/swap/main.jsx
@@ -6,11 +6,14 @@ const SwapPage = () => {
     <div className={styles.main_container}>
       <Navbar/>
         <div className={styles.inner_container}>
+          <Text size={2.5}>We are currently in the private sale phase.  Please join our telegram for more information!</Text>
+          { /* 
           <div className={styles.frame_container}>
-            <Text size={2} margin='0 0 2em 0' weight={1000}>Contract Address:</Text>
-            <Text size={2} margin='-1em 0 2em 0' weight={1000}>-----------------------------------</Text>
-            <iframe src="https://poocoin.app/embed-swap" width="420" height="630"/>
+              <Text size={2} margin='0 0 2em 0' weight={1000}>Contract Address:</Text>
+              <Text size={2} margin='-1em 0 2em 0' weight={1000}>-----------------------------------</Text>
+              <iframe src="https://poocoin.app/embed-swap" width="420" height="630"/> 
           </div>
+          */ }
         </div>
       <Footer/>
     </div>
