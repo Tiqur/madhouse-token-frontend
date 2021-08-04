a1e1cdcd3a7797faeb97289481f6c31340937e09
Tiqur <Tiqur7@gmail.com>
2021-08-03 17:24:35 -0700

    Added telegram link in status

--- src/pages/swap/main.jsx
+++ src/pages/swap/main.jsx
@@ -1,12 +1,15 @@
 import styles from './styles.module.scss';
 import { Navbar, Footer, Text } from '../../components/index.js';
 
+
 const SwapPage = () => {
+  const social_scroll = <a className={styles.scroll_link} target='_blank' href='https://t.me/MadHouseToken'>telegram</a>
+
   return (
     <div className={styles.main_container}>
       <Navbar/>
         <div className={styles.inner_container}>
-          <Text size={2.5}>We are currently in the private sale phase.  Please join our telegram for more information!</Text>
+          <Text size={2.5}>We are currently in the private sale phase.  Please join our {social_scroll} for more information!</Text>
           { /* 
           <div className={styles.frame_container}>
               <Text size={2} margin='0 0 2em 0' weight={1000}>Contract Address:</Text>
