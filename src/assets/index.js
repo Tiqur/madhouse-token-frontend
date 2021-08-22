88e5c3e1286aae58f2abe975b73fa3013b8e86f4
Tiqur <Tiqur7@gmail.com>
2021-08-21 17:07:00 -0700

    Hosted all images locally as webp for better performance

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,8 +7,16 @@ export {ReactComponent as DropSvg} from './drop.svg';
 export {ReactComponent as MoneySvg} from './money.svg';
 export {ReactComponent as VaultSvg} from './vault.svg';
 export {ReactComponent as YoutubeSvg} from './youtube.svg';
+
 export {default as TokenDistribution} from './token_distribution.png';
 export {default as LogoSvg} from './MadHouse.svg';
 export {default as SpinningCoinMP4} from './spinning_coin.mp4';
 export {default as WhitepaperPdf} from './Madhouse-token-lite-paper.pdf';
-export {default as MarkusPng} from './markus.png';
+
+export {default as AltmanWebp} from './altman.webp';
+export {default as BenzolioWebp} from './benzolio.webp';
+export {default as ChichiWebp} from './chichi.webp';
+export {default as LucidWebp} from './lucid.webp';
+export {default as MadfozWebp} from './madfoz.webp';
+export {default as MarkusWebp} from './markus.webp';
+export {default as TiqurWebp} from './tiqur.webp';
