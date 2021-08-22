b94740fb2a33fea1d1ae396b3bcd83a1c2fea123
Tiqur <Tiqur7@gmail.com>
2021-08-21 17:28:17 -0700

    Remove update script

--- update.sh
+++
@@ -1,6 +0,0 @@
-# Update from github and restart
-git stash # Stash local repo
-git pull # Update local repo
-npm install # Update local node modules if necessary
-npm run build # Create optimized build for production
-chmod +x update.sh # Update file permissions 
