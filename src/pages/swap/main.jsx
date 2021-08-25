a2cf96842d7b576677c215d5a526c108e8bdf607
Tiqur <Tiqur7@gmail.com>
2021-08-24 20:41:31 -0700

    Added presale link to swap page

--- src/pages/swap/main.jsx
+++ src/pages/swap/main.jsx
@@ -3,13 +3,14 @@ import { Navbar, Footer, Text } from '../../components/index.js';
 
 
 const SwapPage = () => {
-  const social_scroll = <a className={styles.scroll_link} target='_blank' href='https://t.me/MadHouseToken'>telegram</a>
+  const telemgram_link = <a target='_blank' href='https://t.me/MadHouseToken'>telegram</a>
+  const presale_link = <a target='_blank' href='http://dxsale.app/app/v2_9/defipresale?saleID=2884&chain=BSC'>presale</a>
 
   return (
     <div className={styles.main_container}>
       <Navbar/>
         <div className={styles.inner_container}>
-          <Text size={2.5}>We are currently in the presale whitelisting phase.  Please join our {social_scroll} for more information!</Text>
+          <Text size={2.5}>We are currently in the {presale_link} whitelisting phase.  Please join our {telemgram_link} for more information!</Text>
           { /* 
           <div className={styles.frame_container}>
               <Text size={2} margin='0 0 2em 0' weight={1000}>Contract Address:</Text>
