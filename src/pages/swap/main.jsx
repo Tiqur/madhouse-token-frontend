b31a3257a2938bf600afcf071439d534459b6129
Tiqur <Tiqur7@gmail.com>
2021-09-06 05:51:25 -0700

    Added pancakeswap embed and token address on swap page

--- src/pages/swap/main.jsx
+++ src/pages/swap/main.jsx
@@ -3,21 +3,15 @@ import { Navbar, Footer, Text } from '../../components/index.js';
 
 
 const SwapPage = () => {
-  const telemgram_link = <a target='_blank' href='https://t.me/MadHouseToken'>telegram</a>
-  const presale_link = <a target='_blank' href='http://dxsale.app/app/v2_9/defipresale?saleID=2884&chain=BSC'>presale</a>
-
   return (
     <div className={styles.main_container}>
       <Navbar/>
         <div className={styles.inner_container}>
-          <Text padding='2em 0 0 0' size={2.5}>The {presale_link} is currently active!  Please join our {telemgram_link} for more information!</Text>
-          { /* 
           <div className={styles.frame_container}>
-              <Text size={2} margin='0 0 2em 0' weight={1000}>Contract Address:</Text>
-              <Text size={2} margin='-1em 0 2em 0' weight={1000}>-----------------------------------</Text>
+              <Text size={2} margin='0 0 2em 0' weight={1000}>Token Address:</Text>
+              <Text size={2} margin='-1em 0 2em 0' weight={1000}>0x8c4885867d30f03ad04388cee01c65d11d192e61</Text>
               <iframe src="https://poocoin.app/embed-swap" width="420" height="630"/> 
           </div>
-          */ }
         </div>
       <Footer/>
     </div>
